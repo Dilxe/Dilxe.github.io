ef9840fea04ebc364f0904d4e79202124acf2fe7
Dilxe <44653007+Dilxe@users.noreply.github.com>
2022-01-24 00:10:59 +0000

    Update mailPT.php

--- mailPT.php
+++ mailPT.php
@@ -3,9 +3,9 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "dan.carpe.diem@hotmail.com";
+$recipient = "dilxe.programing@disroot.org";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or header("Location:indexPT.html#erro");
-header("Location:indexPT.html#obrigado");
+header("Location:index.html#obrigado");
 ?>
