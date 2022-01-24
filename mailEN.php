13902e620a2bdef8cb86e8c197dbe0b4cbc46217
Dilxe <44653007+Dilxe@users.noreply.github.com>
2022-01-24 00:10:36 +0000

    Update mailEN.php

--- mailEN.php
+++ mailEN.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "dan.carpe.diem@hotmail.com";
+$recipient = "dilxe.programing@disroot.org";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or header("Location:indexEN.html#error");
